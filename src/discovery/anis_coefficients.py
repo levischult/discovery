e5c8a9c44ab1b70d67948e26b7c959789d6afab4
levi s <lss8ak@virginia.edu>
2025-11-13 10:32:36 -0600

    adding kyle's aniso codes - pixel power basis, jit wrappers

--- src/discovery/anis_coefficients.py
+++ src/discovery/anis_coefficients.py
@@ -2,6 +2,11 @@ import jax.numpy as jnp
 import numpy as np
 import healpy as hp
 import scipy.special as ss
+import discovery.deterministic as dsd
+#from discovery.deterministic import fpc_fast
+from time import time_ns
+import jax
+from jax.scipy import linalg as jsl
 
 
 """
@@ -13,7 +18,306 @@ We have tried to explicitly state which these are using:
 'DIRECT IMPORT FROM ENTERPRISE ANIS_COEF' in the docstring. If function name is
 changed, see the direct import line for the original name.
 
+Conventions:
+Khat - unit vector pointing in the direction of GW propagation (source to observer)
+etahat - unit vector pointing in the direction of the GW source (observer to source)
 """
+
+# Constants
+DAY_SEC = 86400.0  # Number of seconds in a day
+YEAR_SEC = (365.24 * 24 * 3600)  # Number of seconds in a year
+F_YEAR_HZ = 1.0 / YEAR_SEC  # Frequency of 1/year in Hz
+
+C_MPS = 299792458.0  # Speed of light in m/s
+
+PC_M = 3.085677581491367e+16  # Parsec in meters
+KPC_M = PC_M * 1e3  # Kiloparsec in meters
+MPC_M = PC_M * 1e6  # Megaparsec in meters
+
+T_SUN_SEC = 4.9254909476412675e-06 # Solar mass time in seconds
+
+def fpc_fast(pos, gwtheta, gwphi):
+    """Get the Fplus and Fcross antenna response functions
+
+    This function returns a tuple containing the Fplus and Fcross antenna response
+    functions for each pulsar postion and each gw source direction. This function
+    supports vectorization for both positions (N, 3), and gwtheta and gwphi (M)
+
+    Parameters
+    ----------
+    pos : array
+        The array of pulsar position(s). shape=(3) or (N,3)
+    gwtheta : scalar or array
+        The gw origin polar angle(s) for etahat. shape=(1) or (M)
+    gwphi : scalar or array
+        The gw origin azimuthal angle(s) for etahat. shape=(1) or (M)
+
+    Returns
+    -------
+    tuple
+        A tuple containing (Fplus, Fcross) of shape (N,M)
+    """
+    # This bit makes sure the inputs are in the right shape for broadcasting
+    if pos.ndim==2:
+        x, y, z = pos.T # each are shape (N)
+    else:
+        x, y, z = pos[:,None] # each are shape (N=1)
+
+    x, y, z = x[:,None], y[:,None], z[:,None] # Into shape (N,1) 
+    gwtheta = jnp.atleast_1d(gwtheta)[None,:] # (1,M)
+    gwphi = jnp.atleast_1d(gwphi)[None,:] # (1,M)
+
+    # Helpers
+    sin_phi = jnp.sin(gwphi) # (1,M)
+    cos_phi = jnp.cos(gwphi) # (1,M)
+    sin_theta = jnp.sin(gwtheta) # (1,M)
+    cos_theta = jnp.cos(gwtheta) # (1,M)
+
+    # Dot products
+    m_dot_pos = sin_phi*x - cos_phi*y # (N,M)
+    n_dot_pos = -cos_theta*cos_phi*x - cos_theta*sin_phi*y + sin_theta*z # (N,M)
+    omhat_dot_pos = -sin_theta*cos_phi*x - sin_theta*sin_phi*y - cos_theta*z # (N,M)
+
+    denom = 1.0 + omhat_dot_pos # (N,M)
+
+    # Actual fplus and fcross calculations
+    fplus = 0.5 * (m_dot_pos**2 - n_dot_pos**2) / denom # (N,M)
+    fcross = (m_dot_pos * n_dot_pos) / denom # (N,M)
+
+    # Squeeze out any extra dimensions if not vectorized
+    fplus = jnp.squeeze(fplus)
+    fcross = jnp.squeeze(fcross)
+
+    return fplus, fcross # Both are (N,M)
+
+
+
+# Utility functions for other methods
+def spherical2cartesian(theta, phi):
+    """Convert spherical coordinates to unit-radius Cartesian coordinates.
+
+    This function takes spherical coordinates (theta, phi) and converts them
+    to Cartesian coordinates (x, y, z) on the unit sphere.
+
+    This function supports array inputs for theta and phi.
+
+    Parameters
+    ----------
+    theta : float or array_like
+        The polar angle in radians.
+    phi : float or array_like
+        The azimuthal angle in radians.
+
+    Returns
+    -------
+    jnp.ndarray
+        The coordinates in an array of shape (..., 3)
+    """
+    x = jnp.sin(theta) * jnp.cos(phi)
+    y = jnp.sin(theta) * jnp.sin(phi)
+    z = jnp.cos(theta)
+    return jnp.array([x, y, z]).T
+
+
+def cartesian2spherical(pos):
+    """Convert Cartesian coordinates to spherical coordinates.
+
+    This function takes Cartesian coordinates (x, y, z) and converts them
+    to spherical coordinates (theta, phi). This function supports vectorized
+    inputs for pos.
+
+    Parameters
+    ----------
+    pos : array_like
+        The Cartesian coordinates. Shape (N, 3)
+
+    Returns
+    -------
+    tuple
+        The spherical coordinates (theta, phi). Both are of shape (N,)
+    """
+
+    x, y, z = pos.T
+    r = jnp.sqrt(x**2 + y**2 + z**2)
+
+    theta = jnp.arccos(z / r)
+    phi = jnp.arctan2(y, x) % (2 * jnp.pi)
+
+    return theta, phi
+
+
+def pairs2matrix(M):
+    # M has (npairs, N) -> return Mp with (N, npsr, npsr)
+    npair = M.shape[0] # number of pairs
+    nbasis = M.shape[1] # number of basis functions
+    npsr = int(jnp.sqrt(0.25 + 2*npair) - 0.5) # Smaller root of quadratic formula
+
+    Mprime = jnp.zeros((nbasis, npsr, npsr))
+    a,b = jnp.triu_indices(npsr, k=0)
+    Mprime = Mprime.at[:, a, b].set(M.T)
+    Mprime = Mprime.at[:, b, a].set(M.T)
+
+    return Mprime
+
+
+def matrix2pairs(M):
+    # M has shape (N, npsr, npsr) -> return Mp with (npairs, N)
+    npsr = M.shape[1]
+    nbasis = M.shape[0]
+    npair = npsr*(npsr+1)//2
+
+    a,b = jnp.triu_indices(npsr, k=0)
+    Mprime = Mprime.at[:, :].set(M[:, a, b].T)
+
+    return Mprime
+
+
+
+def get_pixel_power_basis(pos, nside=16):
+
+    npsr = pos.shape[0]
+    npix = hp.nside2npix(nside)
+
+    # Define etahat
+    eta_theta, eta_phi = hp.pix2ang(nside, jnp.arange(npix))
+    
+    # Get Fplus and Fcross
+    fp, fc = fpc_fast(pos, eta_theta, eta_phi) # Each are (N, npix)
+
+    # Get the pulsars a and b
+    a,b = jnp.triu_indices(npsr, k=0) # Includes diagonal
+
+    # Build the power basis
+    R = fp[a,:]*fp[b,:] + fc[a,:]*fc[b,:] # Shape (Npairs, npix)
+    # Add normalization
+    R = R * 3/(2*npix) # (Npairs, npix)
+
+    # Convert to square covariance matrix
+    Rcov = pairs2matrix(R)  # Shape (npix, npsr, npsr)
+
+    # Double the diagonal elements for pulsar term
+    idx = jnp.arange(npsr)
+    Rcov = Rcov.at[:, idx, idx].set(2*Rcov[:, idx, idx])
+
+    return Rcov  # Shape (npix, npsr, npsr)
+
+
+def get_pixel_strain_basis(pos, nside=16):
+    npsr = pos.shape[0]
+    npix = hp.nside2npix(nside)
+
+    # Define etahat
+    eta_theta, eta_phi = hp.pix2ang(nside, jnp.arange(npix))
+
+    # Get Fplus and Fcross
+    fp, fc = fpc_fast(pos, eta_theta, eta_phi) # Each are (N, npix)
+
+    F = jnp.zeros((npsr, 2*npix)) # Interleave fplus and fcross
+    F = F.at[:, 0::2].set(fp)
+    F = F.at[:, 1::2].set(fc)
+
+    # Normalize this basis
+    F = F * jnp.sqrt(3/(2*npix)) # (npsr, 2*npix)
+
+    return F  # Shape (npsr, 2*npix)
+
+
+
+
+# Utility functions to keep doc comments when jitting
+def jit_method(func):
+    """A wrapper to JIT compile class methods with a static 'self' as first argument.
+
+    This decorator takes a class method and returns a JIT compiled version of it
+    while preserving the original function's metadata using functools.wraps.
+
+    This is a drop-in replacement for: `@partial(jit, static_argnums=0)`
+
+    Parameters
+    ----------
+    func : callable
+        The function to be wrapped and JIT compiled.
+
+    Returns
+    -------
+    callable
+        The JIT compiled version of the input function.
+    """
+    from jax import jit
+    from functools import wraps
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        jit_func = jit(func, static_argnums=0) # self is static
+        return jit_func(*args, **kwargs)
+    
+    return wrapper
+
+
+def jit_function(func):
+    """A wrapper to JIT compile standalone functions.
+
+    This decorator takes a standalone function and returns a JIT compiled version
+    of it while preserving the original function's metadata using functools.wraps.
+
+    This is a drop-in replacement for: `@jit`
+
+    Parameters
+    ----------
+    func : callable
+        The function to be wrapped and JIT compiled.
+
+    Returns
+    -------
+    callable
+        The JIT compiled version of the input function.
+    """
+    from jax import jit
+    from functools import wraps
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        jit_func = jit(func)
+        return jit_func(*args, **kwargs)
+    
+    return wrapper
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################
 
 def spherical_to_cartesian(theta, phi):
     """
